2595105df1be810782da96ad30f23275455f6366
Ami <117275293+MiguelKummetz@users.noreply.github.com>
2024-10-05 17:46:09 +0200

    database connected and get and post endpoints added

---
+++ src/controllers/fetchQuestion.ts
@@ -0,0 +1,16 @@
+import {Request, Response} from 'express'
+import { Question } from '../database/questionSchema.js'
+
+export const fetchQuestion = async (req: Request, res: Response) => {
+    const {id} = req.params
+    try {
+        const question = await Question.findById(id)
+        if (question) {
+            res.status(200).json(question)
+        }else{
+            res.status(404).json({message: 'Question not found'})
+        }
+    } catch (err) {
+        res.status(500).json({ message: 'Error retrieving the question', error: err });
+    }
+}
