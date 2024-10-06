099abc13e4bf34b3b58d49cb160d84c979aee369
Ami <117275293+MiguelKummetz@users.noreply.github.com>
2024-10-06 03:28:40 +0200

    final touch

--- src/database/models/questionModel.ts
+++
@@ -1,8 +0,0 @@
-export interface IQuestion {
-    statment: string,
-    correctAnswer: { text: string, image: string },
-    wrongAnswer: { text: string, image: string },
-    explanation: { text: string, link: string },
-    audience: string,
-    topic: string
-}
